7eb658c7b2e646976654c5a060891ca813507463
Brad Coughlin <brad@bradcoughlin.com>
2019-06-04 17:27:01 -0700

    CSS module button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button.js';
 
 function App() {
   return (
@@ -8,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CSS Modules Proof-of-Concept
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button></Button>
       </header>
     </div>
   );
